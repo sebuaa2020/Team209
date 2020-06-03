d49f8954004a02d3a3715606dac9e09274e8bfb5
JoJo-js <59838103+JoJo-js@users.noreply.github.com>
2020-06-04 00:18:54 +0800

    Update keyboard_vel_ctrl.cpp

--- code/BasePlate/wpr_simulation/src/keyboard_vel_ctrl.cpp
+++ code/BasePlate/wpr_simulation/src/keyboard_vel_ctrl.cpp
@@ -3,9 +3,14 @@
 #include <stdio.h>
 #include <termios.h>
 
-static float linear_vel = 0.1;
-static float angular_vel = 0.1;
-static int k_vel = 3;
+static double linear_vel = 0.1;
+static double angular_vel = 0.1;
+static int k_vel = 3; //限速
+double speed_x=0,speed_y=0,rotate_z=0,q=0;
+
+struct state{
+	int x,y;
+};
 
 int GetCh()
 {
@@ -16,107 +21,107 @@ int GetCh()
   tcsetattr( STDIN_FILENO, TCSANOW, &newt);
   int c = getchar();
   tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
-  return c;
+  return (char) c;
 }
+
+void control_base(char c){
+	if(c=='w')
+	{
+	  speed_x += linear_vel;
+	} 
+	else if(c=='s')
+	{
+	  speed_x += -linear_vel;
+	} 
+	else if(c=='a')
+	{
+	  speed_y += linear_vel;
+	}
+	else if(c=='d')
+	{
+	  speed_y += -linear_vel;
+	} 
+	else if(c=='q')
+	{
+	  rotate_z += angular_vel;
+	} 
+	else if(c=='e')
+	{
+	  rotate_z += -angular_vel;
+	} 
+	else if(c=='t')
+	{
+	  speed_x = 0;
+	  speed_y = 0;
+	  rotate_z = 0;
+	} 
+	else if(c=='x')
+	{
+	  q = 1;
+	} 
+	else
+	{
+	   printf(" - 未定义指令 \n");
+	}
+}
+
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "keyboard_vel_ctrl");
+	ros::init(argc, argv, "keyboard_vel_ctrl");
+	ros::NodeHandle n;
+	ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+	//ros::Rate loop_rate(10);
+	geometry_msgs::Twist base_cmd;
+	base_cmd.linear.x = 0;
+	base_cmd.linear.y = 0;
+	base_cmd.angular.z = 0;
 
-  printf("键盘控制WPR机器人： \n");
-  printf("w - 向前加速 \n");
-  printf("s - 向后加速 \n");
-  printf("a - 向左加速 \n");
-  printf("d - 向右加速 \n");
-  printf("q - 左旋加速 \n");
-  printf("e - 右旋加速 \n");
-  printf("空格 - 刹车 \n");
-  printf("x - 退出 \n");
-  printf("------------- \n");
-
-  ros::NodeHandle n;
-  ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
-
-  geometry_msgs::Twist base_cmd;
-  base_cmd.linear.x = 0;
-  base_cmd.linear.y = 0;
-  base_cmd.angular.z = 0;
-
-  while(n.ok())
-  {
-    int cKey = GetCh();
-    if(cKey=='w')
-    {
-      base_cmd.linear.x += linear_vel;
-      if(base_cmd.linear.x > linear_vel*k_vel)
-        base_cmd.linear.x = linear_vel*k_vel;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='s')
-    {
-      base_cmd.linear.x += -linear_vel;
-
-      if(base_cmd.linear.x < -linear_vel*k_vel)
-        base_cmd.linear.x = -linear_vel*k_vel;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='a')
-    {
-      base_cmd.linear.y += linear_vel;
-      if(base_cmd.linear.y > linear_vel*k_vel)
-        base_cmd.linear.y = linear_vel*k_vel;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    }
-    else if(cKey=='d')
-    {
-      base_cmd.linear.y += -linear_vel;
-      if(base_cmd.linear.y < -linear_vel*k_vel)
-        base_cmd.linear.y = -linear_vel*k_vel;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='q')
-    {
-      base_cmd.angular.z += angular_vel;
-      if(base_cmd.angular.z > angular_vel*k_vel)
-        base_cmd.angular.z = angular_vel*k_vel;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='e')
-    {
-      base_cmd.angular.z += -angular_vel;
-      if(base_cmd.angular.z < -angular_vel*k_vel)
-        base_cmd.angular.z = -angular_vel*k_vel;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey==' ')
-    {
-      base_cmd.linear.x = 0;
-      base_cmd.linear.y = 0;
-      base_cmd.angular.z = 0;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-    } 
-    else if(cKey=='x')
-    {
-      base_cmd.linear.x = 0;
-      base_cmd.linear.y = 0;
-      base_cmd.angular.z = 0;
-      cmd_vel_pub.publish(base_cmd);
-      printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
-      printf("退出！ \n");
-      return 0;
-    } 
-    else
-    {
-       printf(" - 未定义指令 \n");
-    }
-    
-  }
-  return 0;
-}
+	/*printf("键盘控制WPR机器人： \n");
+	printf("w - 向前加速 \n");
+	printf("s - 向后加速 \n");
+	printf("a - 向左加速 \n");
+	printf("d - 向右加速 \n");
+	printf("q - 左旋加速 \n");
+	printf("e - 右旋加速 \n");
+	printf("t - 刹车 \n");
+	printf("x - 退出 \n");
+	printf("------------- \n");*/
+	
+	while(ros::ok())
+	{
+		char cKey = GetCh();
+		control_base(cKey);
+		if(q==1){
+			speed_x = 0;
+			speed_y = 0;
+			rotate_z = 0;
+			base_cmd.linear.x = speed_x;
+			base_cmd.linear.y = speed_y;
+			base_cmd.angular.z = rotate_z;
+			cmd_vel_pub.publish(base_cmd);
+			printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
+			printf("退出！ \n");
+			return 0;
+		}
+		if(speed_x > linear_vel*k_vel)
+			speed_x = linear_vel*k_vel;
+		if(speed_x < -linear_vel*k_vel)
+			speed_x = -linear_vel*k_vel;
+		if(speed_y > linear_vel*k_vel)
+			speed_y = linear_vel*k_vel;
+		if(speed_y < -linear_vel*k_vel)
+			speed_y = -linear_vel*k_vel;
+		if(rotate_z > angular_vel*k_vel)
+			rotate_z = angular_vel*k_vel;
+		if(rotate_z < -angular_vel*k_vel)
+			rotate_z = -angular_vel*k_vel;
+		base_cmd.linear.x = speed_x;
+		base_cmd.linear.y = speed_y;
+		base_cmd.angular.z = rotate_z;
+		cmd_vel_pub.publish(base_cmd);
+		printf(" - linear.x= %.2f linear.y= %.2f angular.z= %.2f \n",base_cmd.linear.x,base_cmd.linear.y,base_cmd.angular.z);
+		//loop_rate.sleep();
+	}
+	return 0;
+} 
